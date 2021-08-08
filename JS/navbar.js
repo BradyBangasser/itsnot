7f0f257a55fc2f99f690a2befe6909f42856201d
bradyinnovation <bradyincorporated2@gmail.com>
2021-08-08 18:34:16 -0500

    tryserach

--- JS/navbar.js
+++ JS/navbar.js
@@ -19,3 +19,17 @@ $("li.dropdown").mouseleave(function(){
 $(".navbar-toggle").click(function(){
 	$(".navbar-collapse").toggleClass("collapse").slideToggle("fast");
 });
+
+const f = $("daform");
+const s = $("searchbar");
+const google = "https://www.google.com/search?q=site%3A+";
+const site = "itsnotcatchy.com";
+
+function submit(event) {
+	event.preventDefault();
+	const url = google + site + "+" + s.value;
+	const win = window.open(url, '_blank');
+	win.focus();
+}
+
+f.addEventListener("submit", submitted);
