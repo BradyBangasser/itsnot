5feb2883ad9f5df4c8327caef4561519313b2f61
bradyinnovation <bradyincorporated2@gmail.com>
2021-08-12 20:33:59 -0500

    accessportaltesting

--- JS/signup.js
+++ JS/signup.js
@@ -1,4 +1,10 @@
-$(`#submit`).click(function() {
-    alert("poop");
+$('document').ready(function () {
+    $('listplaceholderdiv').load("./accessportal.html ol");
+});
+function denySub() {
+    window.location.href = "http://www.w3schools.com";
     return false;
-})
+};
+function allow() {
+    return true;
+};
