ea258bebed9e8970bd4acc5946016e7e607bdfab
bradyinnovation <bradyincorporated2@gmail.com>
2021-09-07 12:12:23 -0500

    that shoulddoit

--- JS/index.js
+++ JS/index.js
@@ -6,9 +6,9 @@ var x = setInterval(function() {
   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-  $("#count dooku").text(days + "d " + hours + "h " + minutes + "m " + seconds + "s ");
+  $("#countdooku").text(days + "d " + hours + "h " + minutes + "m " + seconds + "s ");
   if (distance < 0) {
     clearInterval(x);
-    $("#count dooku").text("NO MORE SCHOOL YAY");
+    $("#countdooku").text("NO MORE SCHOOL YAY");
   }
 }, 1000);
